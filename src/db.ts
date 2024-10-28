8bde0f850663017b9774fc48b075451bae2cff9a
ram <rammalahi7301@gmail.com>
2024-10-28 11:33:39 +0530

    file

--- src/db.ts
+++ src/db.ts
@@ -1,4 +1,3 @@
-// data.ts
 export const covidData = [
   {
     state: "Maharashtra",
@@ -35,5 +34,207 @@ export const covidData = [
     recovered: 2450000,
     deaths: 30000
   },
-  // Add more states...
+  {
+    state: "Delhi",
+    totalCases: 2000000,
+    activeCases: 15000,
+    recovered: 1970000,
+    deaths: 35000
+  },
+  {
+    state: "West Bengal",
+    totalCases: 1800000,
+    activeCases: 10000,
+    recovered: 1770000,
+    deaths: 20000
+  },
+  {
+    state: "Gujarat",
+    totalCases: 1500000,
+    activeCases: 20000,
+    recovered: 1470000,
+    deaths: 30000
+  },
+  {
+    state: "Rajasthan",
+    totalCases: 1400000,
+    activeCases: 15000,
+    recovered: 1370000,
+    deaths: 25000
+  },
+  {
+    state: "Bihar",
+    totalCases: 1300000,
+    activeCases: 10000,
+    recovered: 1280000,
+    deaths: 20000
+  },
+  {
+    state: "Punjab",
+    totalCases: 1200000,
+    activeCases: 8000,
+    recovered: 1180000,
+    deaths: 14000
+  },
+  {
+    state: "Haryana",
+    totalCases: 1100000,
+    activeCases: 7000,
+    recovered: 1080000,
+    deaths: 13000
+  },
+  {
+    state: "Madhya Pradesh",
+    totalCases: 1000000,
+    activeCases: 12000,
+    recovered: 980000,
+    deaths: 8000
+  },
+  {
+    state: "Chhattisgarh",
+    totalCases: 900000,
+    activeCases: 5000,
+    recovered: 885000,
+    deaths: 10000
+  },
+  {
+    state: "Odisha",
+    totalCases: 850000,
+    activeCases: 6000,
+    recovered: 840000,
+    deaths: 9000
+  },
+  {
+    state: "Telangana",
+    totalCases: 800000,
+    activeCases: 4000,
+    recovered: 790000,
+    deaths: 6000
+  },
+  {
+    state: "Andhra Pradesh",
+    totalCases: 750000,
+    activeCases: 3000,
+    recovered: 740000,
+    deaths: 7000
+  },
+  {
+    state: "Assam",
+    totalCases: 700000,
+    activeCases: 2000,
+    recovered: 695000,
+    deaths: 3000
+  },
+  {
+    state: "Jharkhand",
+    totalCases: 650000,
+    activeCases: 1500,
+    recovered: 645000,
+    deaths: 3500
+  },
+  {
+    state: "Uttarakhand",
+    totalCases: 600000,
+    activeCases: 1000,
+    recovered: 595000,
+    deaths: 4000
+  },
+  {
+    state: "Himachal Pradesh",
+    totalCases: 500000,
+    activeCases: 800,
+    recovered: 495000,
+    deaths: 4200
+  },
+  {
+    state: "Goa",
+    totalCases: 400000,
+    activeCases: 500,
+    recovered: 395000,
+    deaths: 4500
+  },
+  {
+    state: "Puducherry",
+    totalCases: 350000,
+    activeCases: 300,
+    recovered: 345000,
+    deaths: 4700
+  },
+  {
+    state: "Chandigarh",
+    totalCases: 300000,
+    activeCases: 250,
+    recovered: 295000,
+    deaths: 4750
+  },
+  {
+    state: "Manipur",
+    totalCases: 280000,
+    activeCases: 200,
+    recovered: 277000,
+    deaths: 2800
+  },
+  {
+    state: "Meghalaya",
+    totalCases: 250000,
+    activeCases: 150,
+    recovered: 247000,
+    deaths: 2850
+  },
+  {
+    state: "Nagaland",
+    totalCases: 220000,
+    activeCases: 100,
+    recovered: 218000,
+    deaths: 1900
+  },
+  {
+    state: "Tripura",
+    totalCases: 200000,
+    activeCases: 80,
+    recovered: 198000,
+    deaths: 1920
+  },
+  {
+    state: "Mizoram",
+    totalCases: 180000,
+    activeCases: 60,
+    recovered: 178000,
+    deaths: 1940
+  },
+  {
+    state: "Arunachal Pradesh",
+    totalCases: 150000,
+    activeCases: 40,
+    recovered: 149000,
+    deaths: 960
+  },
+  {
+    state: "Sikkim",
+    totalCases: 120000,
+    activeCases: 20,
+    recovered: 119000,
+    deaths: 980
+  },
+  {
+    state: "Ladakh",
+    totalCases: 100000,
+    activeCases: 15,
+    recovered: 99500,
+    deaths: 485
+  },
+  {
+    state: "Lakshadweep",
+    totalCases: 50000,
+    activeCases: 10,
+    recovered: 49800,
+    deaths: 190
+  },
+  {
+    state: "Andaman and Nicobar Islands",
+    totalCases: 30000,
+    activeCases: 5,
+    recovered: 29800,
+    deaths: 195
+  }
 ];
